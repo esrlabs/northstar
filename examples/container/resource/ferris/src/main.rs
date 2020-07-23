413728a5f79562d75f57e263f8c4c8b487e84619
Felix Obenhuber <felix.obenhuber@esrlabs.com>
2020-07-23 11:25:56 +0200

    Simplify resouce examples
    Update resouce example to use a binary from a resouce
    container and not include a bin. Rename resouce containers
    to clarify its purpose.
    TODO: Fix tooling bug for images < 4k block size

---
+++ examples/container/resource/ferris/src/main.rs
@@ -0,0 +1,27 @@
+// Copyright (c) 2019 - 2020 ESRLabs
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+use std::fs;
+use std::io;
+
+fn main() -> io::Result<()> {
+    std::env::args()
+        .nth(0)
+        .ok_or(io::Error::new(
+            io::ErrorKind::Other,
+            "Missing or invalid arguments",
+        ))
+        .map(|greeting| fs::read_to_string(&greeting).unwrap_or(greeting))
+        .and_then(|greeting| ferris_says::say(greeting.as_bytes(), 100, &mut std::io::stdout()))
+}
