dc45e14669e7d02ae7a5fd10fe58b65a92af7f98
Felix Obenhuber <felix.obenhuber@esrlabs.com>
2020-07-27 10:40:57 +0200

    Remove logger from examples. Print to stdout

--- examples/container/memeater/src/main.rs
+++ examples/container/memeater/src/main.rs
@@ -12,17 +12,10 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
-use log::info;
-
 fn main() {
-    logd_logger::builder()
-        .parse_filters("memeater")
-        .tag("memeater")
-        .init();
-
     let mut mem = vec![];
     for _ in 0..9_999_999 {
-        info!("Eating a Megabyte... have {}", mem.len());
+        println!("Eating a Megabyte... have {}", mem.len());
         let mut chunk = vec![];
         for i in 0..1_000_000 {
             chunk.push((i % 8) as u8);
