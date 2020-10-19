f8f5157893407ad115ecfada1a7e2591ce6abbcb
Alfonso Ros <alfonso.ros@esrlabs.com>
2020-10-19 15:07:34 +0200

    Compile tests lib only in linux and android
    solves #125 supersedes #130

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -12,6 +12,8 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
+#![cfg(any(target_os = "linux", target_os = "android"))]
+
 pub mod process_assert;
 pub mod runtime;
 pub mod util;
