8b62e9c779f884a1e5656e1d3f570fdb961bea9d
Felix Obenhuber <felix.obenhuber@esrlabs.com>
2020-11-06 17:18:14 +0100

    Refactor notifications to use a broadcast channel
    Keeping track of subscribptions is anoying. Tokio provides
    a broadcast cheannel that fits well for the usecase. If a client
    is not fast enough processing the events it get's disconnected.

--- north/src/runtime/console.rs
+++ north/src/runtime/console.rs
@@ -34,9 +34,17 @@ use tokio::{
     prelude::*,
     select,
     stream::StreamExt,
-    sync::{self},
+    sync::{self, broadcast},
     task,
 };
+
+// Events from message received by clients in deserialized form
+enum ConnectionEvent {
+    Request(Message),
+    Install(Message, PathBuf),
+}
+
+type NotificationRx = broadcast::Receiver<Notification>;
 
 // Request from the main loop to the console
 #[derive(Debug)]
@@ -51,16 +59,21 @@ pub enum Request {
 pub struct Console {
     event_tx: EventTx,
     address: String,
+    notification_tx: broadcast::Sender<Notification>,
 }
 
 impl Console {
+    /// Construct a new console instance
     pub fn new(address: &str, tx: &EventTx) -> Self {
+        let (notification_tx, _notification_rx) = sync::broadcast::channel(100);
         Self {
             event_tx: tx.clone(),
             address: address.to_owned(),
+            notification_tx,
         }
     }
 
+    /// Process console events
     pub async fn process(
         &self,
         state: &mut State,
@@ -166,19 +179,155 @@ impl Console {
                 error: e,
             })?;
 
+        let notification_tx = self.notification_tx.clone();
         task::spawn(async move {
             // Spawn a task for each incoming connection.
             while let Some(stream) = listener.next().await {
                 if let Ok(stream) = stream {
                     let event_tx = event_tx.clone();
+                    let notification_rx = notification_tx.subscribe();
                     task::spawn(async move {
-                        if let Err(e) = connection(stream, event_tx).await {
+                        if let Err(e) = Self::connection(stream, event_tx, notification_rx).await {
                             warn!("Error servicing connection: {}", e);
                         }
                     });
                 }
             }
         });
+        Ok(())
+    }
+
+    /// Send a notification to the notification broadcast
+    pub async fn notification(&self, notification: Notification) -> Result<(), Error> {
+        self.notification_tx
+            .send(notification)
+            .map_err(|_| Error::Internal("Notification channel tx"))
+            .map(drop)
+    }
+
+    async fn connection(
+        stream: TcpStream,
+        event_tx: EventTx,
+        mut notification_rx: NotificationRx,
+    ) -> Result<(), Error> {
+        let peer = stream.peer_addr().map_err(|e| Error::Io {
+            context: "Failed to get peer from command connection".to_string(),
+            error: e,
+        })?;
+        debug!("Client {:?} connected", peer);
+
+        let tmpdir = tempdir().map_err(|e| Error::Io {
+            context: "Error creating temp installation dir".to_string(),
+            error: e,
+        })?;
+
+        let dir = tmpdir.path().to_owned();
+
+        let (reader, mut writer) = stream.into_split();
+
+        // RX
+        let mut client_in = {
+            let (tx, rx) = mpsc::channel(10);
+            task::spawn(async move {
+                let mut reader = BufReader::new(reader);
+                loop {
+                    match read(&mut reader, &dir).await {
+                        Ok(event) => {
+                            if tx.send(event).await.is_err() {
+                                break;
+                            }
+                        }
+                        Err(e) => {
+                            warn!("Error receiving from socket: {}", e);
+                            break;
+                        }
+                    }
+                }
+            });
+            rx
+        };
+
+        // TX
+        let client_out = {
+            let (tx, mut rx_messages) = mpsc::channel::<Message>(1);
+            task::spawn(async move {
+                async fn send<W: Unpin + AsyncWrite>(
+                    reply: &Message,
+                    writer: &mut W,
+                ) -> io::Result<()> {
+                    // Serialize reply
+                    let reply = serde_json::to_string_pretty(&reply)
+                        .map_err(|e| io::Error::new(ErrorKind::Other, e))?;
+
+                    // Send reply
+                    let mut buffer = [0u8; 4];
+                    BigEndian::write_u32(&mut buffer, reply.len() as u32);
+                    writer.write_all(&buffer).await?;
+                    writer.write_all(reply.as_bytes()).await?;
+                    Ok(())
+                }
+
+                loop {
+                    select! {
+                        message = rx_messages.recv() => {
+                            if let Some(message) = message {
+                                if let Err(e) = send(&message, &mut writer).await {
+                                    // TODO: Is the connection closed if this happens?
+                                    warn!("Error sending back to client: {}", e);
+                                    break;
+                                }
+                            } else {
+                                break;
+                            }
+                        }
+                        notification = notification_rx.recv() => {
+                            let payload = match notification {
+                                Ok(notification) => notification,
+                                Err(broadcast::error::RecvError::Closed) => break,
+                                Err(broadcast::error::RecvError::Lagged(_)) => {
+                                    warn!("Client connection lagged notifications. Closing");
+                                    break;
+                                }
+                            };
+
+                            let message = Message {
+                                id: uuid::Uuid::new_v4().to_string(),
+                                payload: Payload::Notification(payload),
+                            };
+                            if send(&message, &mut writer).await.is_err() {
+                                break;
+                            }
+                        }
+                    }
+                }
+            });
+            tx
+        };
+
+        while let Some(request) = client_in.next().await {
+            let request = match request {
+                ConnectionEvent::Request(request) => Request::Message(request),
+                ConnectionEvent::Install(message, npk) => Request::Install(message, npk),
+            };
+            let (reply_tx, mut reply_rx) = mpsc::channel::<Message>(1);
+            let event = Event::Console(request, reply_tx);
+            event_tx
+                .send(event)
+                .await
+                .expect("Internal channel error on main");
+
+            let reply = reply_rx
+                .recv()
+                .await
+                .expect("Internal channel error on client reply");
+
+            if client_out.send(reply).await.is_err() {
+                break;
+            }
+        }
+
+        debug!("Connection to {} closed", peer);
+
         Ok(())
     }
 }
@@ -242,13 +391,13 @@ async fn read<R: AsyncRead + Unpin>(
         .read_exact(&mut buffer)
         .await
         .map_err(|e| Error::Io {
-            context: "Failed to read connection".to_string(),
+            context: "Failed to read payload".to_string(),
             error: e,
         })?;
 
     // Deserialize message
     let message: Message = serde_json::from_slice(&buffer)
-        .map_err(|_| Error::Protocol("Failed to parse protocol message".to_string()))?;
+        .map_err(|_| Error::Protocol("Failed to deserialize message".to_string()))?;
 
     match &message.payload {
         Payload::Installation(size) => {
@@ -279,154 +428,4 @@ async fn read<R: AsyncRead + Unpin>(
         }
         _ => Ok(ConnectionEvent::Request(message)),
     }
-}
-
-enum ConnectionEvent {
-    Request(Message),
-    Install(Message, PathBuf),
-}
-
-async fn connection(stream: TcpStream, event_tx: EventTx) -> Result<(), Error> {
-    let subscription_id = uuid::Uuid::new_v4().to_string();
-    let peer = stream.peer_addr().map_err(|e| Error::Io {
-        context: "Failed to get peer from command connection".to_string(),
-        error: e,
-    })?;
-    debug!("Client {:?} connected", peer);
-
-    let tmpdir = tempdir().map_err(|e| Error::Io {
-        context: "Error creating temp installation dir".to_string(),
-        error: e,
-    })?;
-
-    // For each new client connection we create a new channel
-    // the rx end is used to report notifications to the client
-    // while the tx end is stored in the runtime to broadcast notifications
-    let (notification_sender, mut notifications) = mpsc::channel::<Notification>(10);
-    event_tx
-        .send(Event::NotificationSubscription {
-            id: subscription_id.clone(),
-            subscriber: Some(notification_sender),
-        })
-        .await
-        .map_err(|_| Error::Internal("Channel"))?;
-
-    let dir = tmpdir.path().to_owned();
-
-    let (reader, mut writer) = stream.into_split();
-
-    // RX
-    let mut client_in = {
-        let (tx, rx) = mpsc::channel(10);
-        task::spawn(async move {
-            let mut reader = BufReader::new(reader);
-            loop {
-                match read(&mut reader, &dir).await {
-                    Ok(event) => {
-                        if tx.send(event).await.is_err() {
-                            break;
-                        }
-                    }
-                    Err(e) => {
-                        warn!("Error receiving from socket: {}", e);
-                        break;
-                    }
-                }
-            }
-        });
-        rx
-    };
-
-    // TX
-    let client_out = {
-        let (tx, mut rx) = mpsc::channel::<Message>(1);
-        task::spawn(async move {
-            async fn send_reply<W: Unpin + AsyncWrite>(
-                reply: &Message,
-                writer: &mut W,
-            ) -> io::Result<()> {
-                // Serialize reply
-                let reply = serde_json::to_string_pretty(&reply)
-                    .map_err(|e| io::Error::new(ErrorKind::Other, e))?;
-
-                // Send reply
-                let mut buffer = [0u8; 4];
-                BigEndian::write_u32(&mut buffer, reply.len() as u32);
-                writer.write_all(&buffer).await?;
-                writer.write_all(reply.as_bytes()).await?;
-                Ok(())
-            }
-            while let Some(msg_to_send) = rx.recv().await {
-                if let Err(e) = send_reply(&msg_to_send, &mut writer).await {
-                    // TODO: Is the connection closed if this happens?
-                    warn!("Error sending back to client: {}", e);
-                    break;
-                }
-            }
-        });
-        tx
-    };
-
-    loop {
-        select! {
-            notification = notifications.next() => {
-                if let Some(notification) = notification {
-                    let reply = Message {
-                        id: uuid::Uuid::new_v4().to_string(),
-                        payload: Payload::Notification(notification),
-                    };
-                    // If there's a error on the response tx indicates
-                    // that the connection is closed. Break.
-                    if client_out.send(reply).await.is_err() {
-                        break;
-                    }
-                } else {
-                    break;
-                }
-            }
-            client_in = client_in.next() => {
-                match client_in {
-                    Some(request) => {
-                        let request = match request {
-                            ConnectionEvent::Request(request) => Request::Message(request),
-                            ConnectionEvent::Install(message, npk) => Request::Install(message, npk),
-                        };
-                        let (reply_tx, mut reply_rx) = mpsc::channel::<Message>(1);
-                        let event = Event::Console(request, reply_tx);
-                        event_tx
-                            .send(event)
-                            .await
-                            .expect("Internal channel error on main");
-
-                        // Wait for reply of the main loop
-                        // TODO: Add a timeout to not make the connection wait forever
-                        let reply = reply_rx
-                            .recv()
-                            .await
-                            .expect("Internal channel error on client reply");
-
-                        if client_out.send(reply).await.is_err() {
-                            break;
-                        }
-                    }
-                    None => {
-                        debug!("Client disconnected");
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
-    debug!("Connection to {} closed", peer);
-
-    event_tx
-        .send(Event::NotificationSubscription {
-            subscriber: None,
-            id: subscription_id,
-        })
-        .await
-        .expect("Internal channel error on main");
-
-    Ok(())
 }
