95e4c2da3ec6b7e5d7456264a400626702ff4947
Felix Obenhuber <felix.obenhuber@esrlabs.com>
2020-11-20 14:02:18 +0100

    Refactor manifest mounts
    The mount flags on a persist mount do not make sense since this
    shall always be rw. Change this to
    
    ```
    /foo: persist
    ```
    
    A mount of /dev can only be on /dev and is default with a minimal
    set of devices (done by minijail). Optionally a mount entry
    
    ```
    /dev: full
    ```
    
    does a full fledged rw bind mount of `/dev` if more than the minimal
    set is needed.

--- npk/src/manifest.rs
+++ npk/src/manifest.rs
@@ -20,7 +20,7 @@ use serde::{
 };
 use std::{
     collections::{HashMap, HashSet},
-    fmt,
+    fmt, io,
     path::{Path, PathBuf},
     str::FromStr,
 };
@@ -140,9 +140,6 @@ pub enum MountFlag {
 /// Configuration for the /dev mount
 #[derive(Clone, Eq, PartialEq, Debug, Serialize, Deserialize)]
 pub enum Dev {
-    /// Use minijail minimal /dev tmps meachnism
-    #[serde(rename = "minimal")]
-    Minimal,
     /// Bind mount the full /dev of the host
     #[serde(rename = "full")]
     Full,
@@ -164,10 +161,10 @@ pub enum Mount {
     },
     /// Mount /dev with flavor `dev`
     Dev { r#type: Dev },
-    /// Mount a host directory optionally RW to `target`
-    Persist { flags: HashSet<MountFlag> },
+    /// Mount a rw host directory dedicated to this container rw
+    Persist,
     /// Mount a tmpfs with size
-    Tmpfs { size: String },
+    Tmpfs { size: u64 },
 }
 
 #[derive(Clone, Eq, PartialEq, Debug, Serialize, Deserialize)]
@@ -210,6 +207,13 @@ pub struct Manifest {
     pub mounts: HashMap<PathBuf, Mount>,
 }
 
+/// Configuration for the persist mounts
+#[derive(Clone, Eq, PartialEq, Debug, Serialize, Deserialize)]
+pub enum Persist {
+    #[serde(rename = "persist")]
+    Persist,
+}
+
 struct MountsSerialization;
 
 #[derive(Clone, Eq, PartialEq, Debug, Serialize, Deserialize)]
@@ -219,50 +223,68 @@ enum MountSource {
         resource: String,
     },
     Tmpfs {
-        #[serde(deserialize_with = "deserialize_tmpfs_size")]
-        size: String,
+        #[serde(deserialize_with = "deserialize_tmpfs")]
+        tmpfs: u64,
     },
     Bind {
         host: PathBuf,
-        #[serde(default)]
+        #[serde(default, skip_serializing_if = "HashSet::is_empty")]
         flags: HashSet<MountFlag>,
     },
     Dev(Dev),
-    Persist {
-        #[serde(default)]
-        flags: HashSet<MountFlag>,
-    },
+    Persist(Persist),
 }
 
-fn deserialize_tmpfs_size<'de, D>(deserializer: D) -> Result<String, D::Error>
+fn deserialize_tmpfs<'de, D>(deserializer: D) -> Result<u64, D::Error>
 where
     D: Deserializer<'de>,
 {
     struct SizeVisitor;
 
     impl<'de> Visitor<'de> for SizeVisitor {
-        type Value = String;
+        type Value = u64;
         fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
             formatter.write_str("a number of bytes or a string with the size (e.g. 25M)")
         }
 
         fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E> {
-            Ok(format!("{}", v))
+            Ok(v)
         }
 
         fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
         where
             E: serde::de::Error,
         {
-            // check if the string is correctly formatted as size in tmpfs(5)
             lazy_static! {
-                static ref TMPFS_SIZE: regex::Regex =
-                    regex::Regex::new(r"^\d+(?i:k|m|g)?$").expect("Wrong regex");
+                static ref RE: regex::Regex =
+                    regex::Regex::new(r"^(?P<value>\d+)(?P<unit>k|m|g)?$").expect("Invalid regex");
             }
-            if TMPFS_SIZE.is_match(v) {
-                Ok(v.to_owned())
+
+            let caps = RE
+                .captures(&v)
+                .ok_or_else(|| serde::de::Error::custom(format!("Invalid tmpfs size: {}", v)))?;
+
+            let value = caps
+                .name("value")
+                .unwrap()
+                .as_str()
+                .parse::<u64>()
+                .map_err(serde::de::Error::custom)?;
+            if let Some(unit) = caps.name("unit") {
+                let value = match unit.as_str() {
+                    "k" => value * 1024,
+                    "m" => value * 1024 * 1024,
+                    "g" => value * 1024 * 1024 * 1024,
+                    _ => {
+                        return Err(serde::de::Error::custom(format!(
+                            "Invalid tmpfs unit: {}",
+                            unit.as_str()
+                        )))
+                    }
+                };
+                Ok(value)
             } else {
-                Err(E::custom("Wrong format for tmpfs size parameter"))
+                Ok(value)
             }
         }
     }
@@ -277,30 +299,41 @@ impl MountsSerialization {
     {
         let mut map = serializer.serialize_map(Some(mounts.len()))?;
         for (target, mount) in mounts {
-            match mount {
-                Mount::Bind { host, flags } => map.serialize_entry(
-                    &target,
-                    &MountSource::Bind {
-                        host: host.clone(),
-                        flags: flags.clone(),
-                    },
-                )?,
-                Mount::Dev { r#type } => map.serialize_entry(&target, &r#type)?,
-                Mount::Persist { flags } => map.serialize_entry(
-                    &target,
-                    &MountSource::Persist {
-                        flags: flags.clone(),
-                    },
-                )?,
-                Mount::Resource { name, version, dir } => map.serialize_entry(
-                    &target,
-                    &MountSource::Resource {
-                        resource: format!("{}:{}{}", name, version, dir.display()),
-                    },
-                )?,
-                Mount::Tmpfs { size } => {
-                    map.serialize_entry(&target, &MountSource::Tmpfs { size: size.clone() })?
+            match target.display().to_string().as_str() {
+                "/dev" => {
+                    if let Mount::Dev { r#type } = mount {
+                        map.serialize_entry(&target, &r#type)?;
+                    } else {
+                        return Err(serde::ser::Error::custom("Invalid mount type on /dev"));
+                    }
                 }
+                target => match mount {
+                    Mount::Bind { host, flags } => map.serialize_entry(
+                        &target,
+                        &MountSource::Bind {
+                            host: host.clone(),
+                            flags: flags.clone(),
+                        },
+                    )?,
+                    Mount::Dev { r#type: _ } => {
+                        return Err(serde::ser::Error::custom(format!(
+                            "dev cannot be mounted on {}",
+                            target
+                        )));
+                    }
+                    Mount::Persist => {
+                        map.serialize_entry(&target, &MountSource::Persist(Persist::Persist))?
+                    }
+                    Mount::Resource { name, version, dir } => map.serialize_entry(
+                        &target,
+                        &MountSource::Resource {
+                            resource: format!("{}:{}{}", name, version, dir.display()),
+                        },
+                    )?,
+                    Mount::Tmpfs { size } => {
+                        map.serialize_entry(&target, &MountSource::Tmpfs { tmpfs: *size })?
+                    }
+                },
             }
         }
         map.end()
@@ -320,23 +353,39 @@ impl MountsSerialization {
             {
                 let mut entries = HashMap::<PathBuf, Mount>::new();
                 while let Some((target, source)) = map.next_entry()? {
-                    if entries.contains_key(&target) {
-                        return Err(serde::de::Error::custom(format!(
-                            "Duplicate mountpoint: {:?}",
-                            target
-                        )));
-                    }
-                    entries.insert(
-                        target,
-                        match source {
+                    let target: PathBuf = target;
+                    let mount = match target.display().to_string().as_str() {
+                        "/dev" => {
+                            if let MountSource::Dev(dev) = source {
+                                Mount::Dev { r#type: dev }
+                            } else {
+                                return Err(serde::de::Error::custom(format!(
+                                    "Invalid mount on /dev: {:?}",
+                                    source
+                                )));
+                            }
+                        }
+                        _ => match source {
                             MountSource::Bind { host, flags } => Mount::Bind { host, flags },
-                            MountSource::Dev(dev) => Mount::Dev { r#type: dev },
-                            MountSource::Tmpfs { size } => Mount::Tmpfs { size },
-                            MountSource::Persist { flags } => Mount::Persist { flags },
+                            MountSource::Dev(..) => {
+                                return Err(serde::de::Error::custom(format!(
+                                    "dev cannot be mounted on {}",
+                                    target.display()
+                                )));
+                            }
+                            MountSource::Tmpfs { tmpfs: size } => Mount::Tmpfs { size },
+                            MountSource::Persist(Persist::Persist) => {
+                                if entries.values().any(|v| v == &Mount::Persist) {
+                                    return Err(serde::de::Error::custom(
+                                        "mount configurations can only have one persist entry",
+                                    ));
+                                }
+                                Mount::Persist
+                            }
                             MountSource::Resource { resource } => {
                                 lazy_static! {
                                     static ref RE: regex::Regex = regex::Regex::new(
-                                        r"(?P<name>\w+):(?P<version>[\d.]+)(?P<dir>[\w/]+)?"
+                                        r"(?P<name>((\w|-|\.|_)+)):(?P<version>\d+\.\d+\.\d+)(?P<dir>[\w/]+)"
                                     )
                                     .expect("Invalid regex");
                                 }
@@ -352,13 +401,18 @@ impl MountsSerialization {
                                 let version =
                                     Version::parse(caps.name("version").unwrap().as_str())
                                         .map_err(serde::de::Error::custom)?;
-                                let dir =
-                                    PathBuf::from(caps.name("dir").map_or("/", |m| m.as_str()));
+                                let dir = PathBuf::from(caps.name("dir").unwrap().as_str());
 
                                 Mount::Resource { name, version, dir }
                             }
                         },
-                    );
+                    };
+                    if entries.insert(target.clone(), mount).is_some() {
+                        return Err(serde::de::Error::custom(format!(
+                            "Duplicate mountpoint: {:?}",
+                            target
+                        )));
+                    }
                 }
                 Ok(entries)
             }
@@ -375,16 +429,18 @@ impl MountsSerialization {
 #[derive(Error, Debug)]
 pub enum ManifestError {
     #[error("Invalid manifest ({0})")]
-    MalformedManifest(String),
-    #[error("Missing attribute: {0}")]
-    CouldNotParse(String),
+    Invalid(String),
+    #[error("Failed to parse: {0}")]
+    Parse(#[from] serde_yaml::Error),
+    #[error("IO: {0}")]
+    Io(#[from] io::Error),
 }
 
 impl Manifest {
     fn verify(&self) -> Result<(), ManifestError> {
         // TODO: check for none on env, autostart, cgroups, seccomp, instances
         if self.init.is_none() && self.args.is_some() {
-            return Err(ManifestError::MalformedManifest(
+            return Err(ManifestError::Invalid(
                 "Arguments not allowed in resource container".to_string(),
             ));
         }
@@ -393,9 +449,7 @@ impl Manifest {
 
     pub async fn from_path(f: &Path) -> Result<Manifest, ManifestError> {
         let f = f.to_owned();
-        let manifest = fs::read_to_string(&f).await.map_err(|_| {
-            ManifestError::CouldNotParse(format!("Failed to read manifest from {}", f.display()))
-        })?;
+        let manifest = fs::read_to_string(&f).await.map_err(ManifestError::Io)?;
         let manifest: Manifest = Manifest::from_str(&manifest)?;
         manifest.verify()?;
         Ok(manifest)
@@ -410,8 +464,8 @@ impl Manifest {
 impl FromStr for Manifest {
     type Err = ManifestError;
     fn from_str(s: &str) -> Result<Manifest, ManifestError> {
-        let parse_res: Result<Manifest, ManifestError> = serde_yaml::from_str(s)
-            .map_err(|_| ManifestError::CouldNotParse("Failed to parse manifest".to_string()));
+        let parse_res: Result<Manifest, ManifestError> =
+            serde_yaml::from_str(s).map_err(ManifestError::Parse);
         if let Ok(manifest) = &parse_res {
             manifest.verify()?;
         }
@@ -424,37 +478,28 @@ mod tests {
     use crate::manifest::*;
     use anyhow::{anyhow, Result};
 
-    #[tokio::test]
-    async fn parse() -> Result<()> {
-        use std::{fs::File, io::Write, path::PathBuf};
-
-        let file = tempfile::NamedTempFile::new()?;
-        let path = file.path();
-        let m = "
+    #[test]
+    fn parse() -> Result<()> {
+        let manifest = "
 name: hello
 version: 0.0.0
 init: /binary
 args:
-    - one
-    - two
+  - one
+  - two
 env:
-    LD_LIBRARY_PATH: /lib
+  LD_LIBRARY_PATH: /lib
 mounts:
-    /lib:
-      host: /lib
-      flags:
-          - rw
-    /data: {}
-    /data_rw:
-      flags:
-          - rw
-    /resource:
-        resource: bla:1.0.0/bin/foo
-    /tmpfs:
-        size: 42
-    /big_tmpfs:
-        size: 42G
-    /dev: minimal
+  /tmp:
+    tmpfs: 42
+  /dev: full
+  /lib:
+    host: /lib
+    flags:
+      - rw
+  /data: persist
+  /resource:
+    resource: bla-blah.foo:1.0.0/bin/foo
 autostart: true
 cgroups:
   mem:
@@ -470,11 +515,7 @@ log:
         custom: 8
 ";
 
-        let mut file = File::create(path)?;
-        file.write_all(m.as_bytes())?;
-        drop(file);
-
-        let manifest = Manifest::from_path(&PathBuf::from(path)).await?;
+        let manifest = Manifest::from_str(&manifest)?;
 
         assert_eq!(manifest.init, Some(PathBuf::from("/binary")));
         assert_eq!(manifest.name, "hello");
@@ -497,44 +538,17 @@ log:
                 flags: [MountFlag::Rw].iter().cloned().collect(),
             },
         );
-        mounts.insert(
-            PathBuf::from("/data"),
-            Mount::Persist {
-                flags: HashSet::new(),
-            },
-        );
-        mounts.insert(
-            PathBuf::from("/data_rw"),
-            Mount::Persist {
-                flags: [MountFlag::Rw].iter().cloned().collect(),
-            },
-        );
+        mounts.insert(PathBuf::from("/data"), Mount::Persist);
         mounts.insert(
             PathBuf::from("/resource"),
             Mount::Resource {
-                name: "bla".to_string(),
+                name: "bla-blah.foo".to_string(),
                 version: Version::parse("1.0.0")?,
                 dir: PathBuf::from("/bin/foo"),
             },
         );
-        mounts.insert(
-            PathBuf::from("/tmpfs"),
-            Mount::Tmpfs {
-                size: "42".to_string(),
-            },
-        );
-        mounts.insert(
-            PathBuf::from("/big_tmpfs"),
-            Mount::Tmpfs {
-                size: "42G".to_string(),
-            },
-        );
-        mounts.insert(
-            PathBuf::from("/dev"),
-            Mount::Dev {
-                r#type: Dev::Minimal,
-            },
-        );
+        mounts.insert(PathBuf::from("/tmp"), Mount::Tmpfs { size: 42 });
+        mounts.insert(PathBuf::from("/dev"), Mount::Dev { r#type: Dev::Full });
         assert_eq!(manifest.mounts, mounts);
         assert_eq!(
             manifest.cgroups,
@@ -552,28 +566,92 @@ log:
         Ok(())
     }
 
+    /// Two mounts on the same target are invalid
     #[tokio::test]
-    async fn dupllicate_mount() -> Result<()> {
-        use std::{fs::File, io, path::PathBuf};
-
-        let file = tempfile::NamedTempFile::new()?;
-        let path = file.path();
-        let m = "
+    async fn duplicate_mount() -> Result<()> {
+        let manifest = "
 name: hello
 version: 0.0.0
 init: /binary
 mounts:
-    /dev: minimal
-    /dev: full 
+  /dev: full 
+  /dev: full 
 ";
-
-        let mut file = File::create(path)?;
-        io::copy(&mut m.as_bytes(), &mut file)?;
-        drop(file);
-
-        assert!(Manifest::from_path(&PathBuf::from(path)).await.is_err());
+        assert!(Manifest::from_str(manifest).is_err());
 
         Ok(())
+    }
+
+    #[test]
+    fn tmpfs() {
+        let manifest = "
+name: hello
+version: 0.0.0
+init: /binary
+mounts:
+  /a:
+    tmpfs: 100
+  /b:
+    tmpfs: 100k
+  /c:
+    tmpfs: 100m
+  /d:
+    tmpfs: 100g
+";
+        let manifest = Manifest::from_str(manifest).unwrap();
+        assert!(manifest.mounts.get(&PathBuf::from("/a")) == Some(&Mount::Tmpfs { size: 100 }));
+        assert!(
+            manifest.mounts.get(&PathBuf::from("/b")) == Some(&Mount::Tmpfs { size: 100 * 1024 })
+        );
+        assert!(
+            manifest.mounts.get(&PathBuf::from("/c"))
+                == Some(&Mount::Tmpfs {
+                    size: 100 * 1024 * 1024
+                })
+        );
+        assert!(
+            manifest.mounts.get(&PathBuf::from("/d"))
+                == Some(&Mount::Tmpfs {
+                    size: 100 * 1024 * 1024 * 1024
+                })
+        );
+
+        // Test a invalid tmpfs size string
+        let manifest = "
+name: hello
+version: 0.0.0
+init: /binary
+mounts:
+  /tmp:
+    tmpfs: 100M
+";
+        assert!(Manifest::from_str(manifest).is_err());
+    }
+
+    #[test]
+    fn invalid_dev() {
+        let manifest = "
+name: hello
+version: 0.0.0
+init: /binary
+mounts:
+  /dev:
+    tmpfs: 42
+";
+        assert!(Manifest::from_str(manifest).is_err());
+    }
+
+    #[test]
+    fn mount_ressource() {
+        let manifest = "
+name: hello
+version: 0.0.0
+init: /binary
+mounts:
+  /foo:
+    resource: foo-bar.qwerty12:0.0.1/
+";
+        Manifest::from_str(manifest).unwrap();
     }
 
     #[test]
@@ -583,26 +661,21 @@ name: hello
 version: 0.0.0
 init: /binary
 args:
-    - one
-    - two
+  - one
+  - two
 env:
-    LD_LIBRARY_PATH: /lib
+  LD_LIBRARY_PATH: /lib
 mounts:
-    /lib:
-      host: /lib
-      flags:
-          - rw
-    /data: {}
-    /data_rw:
-      flags:
-          - rw
-    /resource:
-        resource: bla:1.0.0/bin/foo
-    /tmpfs:
-        size: 42
-    /big_tmpfs:
-        size: 42G
-    /dev: minimal
+  /lib:
+    host: /lib
+    flags:
+      - rw
+  /data: persist
+  /resource:
+    resource: bla-bar.blah1234:1.0.0/bin/foo
+  /tmp:
+    tmpfs: 42
+  /dev: full
 autostart: true
 cgroups:
   mem:
@@ -610,12 +683,12 @@ cgroups:
   cpu:
     shares: 100
 seccomp:
-    fork: 1
-    waitpid: 1
+  fork: 1
+  waitpid: 1
 log:
-    tag: test
-    buffer:
-        custom: 8
+  tag: test
+  buffer:
+    custom: 8
 ";
 
         let manifest = serde_yaml::from_str::<Manifest>(m)?;
