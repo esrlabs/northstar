2874932f4280f89301fcc66dfd8247f53071bbd1
Felix Obenhuber <felix.obenhuber@esrlabs.com>
2021-01-08 11:26:03 +0100

    Move formating routines into dedicated module

---
+++ nstar/src/pretty.rs
@@ -0,0 +1,106 @@
+// Copyright (c) 2021 ESRLabs
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+use anyhow::Result;
+use itertools::Itertools;
+use northstar::{api::model::{Container, Notification, Repository}, runtime::RepositoryId};
+use prettytable::{format, Attr, Cell, Row, Table};
+use std::{collections::HashMap, io};
+use tokio::time;
+
+pub fn notification<W: io::Write>(mut w: W, notification: &Notification) {
+    let msg = format!("--> {:?}", notification);
+    writeln!(w, "{}", msg).ok();
+}
+
+pub fn containers<W: io::Write>(mut w: W, containers: &[Container]) -> Result<()> {
+    let mut table = Table::new();
+    table.set_format(*format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
+    table.set_titles(Row::new(vec![
+        Cell::new("Name").with_style(Attr::Bold),
+        Cell::new("Version").with_style(Attr::Bold),
+        Cell::new("Repository").with_style(Attr::Bold),
+        Cell::new("Type").with_style(Attr::Bold),
+        Cell::new("PID").with_style(Attr::Bold),
+        Cell::new("Uptime").with_style(Attr::Bold),
+    ]));
+    for container in containers
+        .iter()
+        .sorted_by_key(|c| &c.manifest.name) // Sort by name
+        .sorted_by_key(|c| c.manifest.init.is_none())
+    {
+        table.add_row(Row::new(vec![
+            Cell::new(&container.manifest.name).with_style(Attr::Bold),
+            Cell::new(&container.manifest.version.to_string()),
+            Cell::new(&container.repository),
+            Cell::new(
+                container
+                    .manifest
+                    .init
+                    .as_ref()
+                    .map(|_| "App")
+                    .unwrap_or("Resource"),
+            ),
+            Cell::new(
+                &container
+                    .process
+                    .as_ref()
+                    .map(|p| p.pid.to_string())
+                    .unwrap_or_default(),
+            )
+            .with_style(Attr::ForegroundColor(prettytable::color::GREEN)),
+            Cell::new(
+                &container
+                    .process
+                    .as_ref()
+                    .map(|p| format!("{:?}", time::Duration::from_nanos(p.uptime)))
+                    .unwrap_or_default(),
+            ),
+        ]));
+    }
+
+    print_table(&mut w, &table)?;
+    w.flush()?;
+    Ok(())
+}
+
+pub fn repositories<W: io::Write>(
+    mut w: W,
+    repositories: &HashMap<RepositoryId, Repository>,
+) -> Result<()> {
+    let mut table = Table::new();
+    table.set_format(*format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
+    table.set_titles(Row::new(vec![
+        Cell::new("Name").with_style(Attr::Bold),
+        Cell::new("Path").with_style(Attr::Bold),
+    ]));
+    for (id, repo) in repositories.iter().sorted_by_key(|(i, _)| (*i).clone())
+    // Sort by name
+    {
+        table.add_row(Row::new(vec![
+            Cell::new(&id).with_style(Attr::Bold),
+            Cell::new(&repo.dir.display().to_string()),
+        ]));
+    }
+
+    print_table(&mut w, &table)?;
+    w.flush()?;
+    Ok(())
+}
+
+fn print_table<W: std::io::Write>(mut w: W, table: &Table) -> Result<()> {
+    w.write_all(table.to_string().as_bytes())?;
+    w.write_all(b"\n")?;
+    Ok(())
+}
