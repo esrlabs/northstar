96f2d5a1adff800346fc10af33dc4d169f48b8ca
Felix Obenhuber <felix.obenhuber@esrlabs.com>
2021-01-14 14:03:03 +0100

    Minor cleanup of resource hello example
    Remove the newline in the message file and remove a
    unwrap in the example code.

--- examples/container/resource/resource_hello/src/main.rs
+++ examples/container/resource/resource_hello/src/main.rs
@@ -16,16 +16,10 @@ use std::{fs, io, path::PathBuf};
 fn main() -> io::Result<()> {
     for i in 0..u64::MAX {
         for entry in fs::read_dir(PathBuf::from("/message"))? {
-            let entry = entry?;
-            let path = entry.path();
+            let path = entry?.path();
             if path.is_file() {
-                let resource_content = fs::read_to_string(&path).unwrap();
-                println!(
-                    "{}: Content of {}: {}",
-                    i,
-                    path.to_string_lossy(),
-                    resource_content
-                );
+                let resource_content = fs::read_to_string(&path)?;
+                println!("{}: Content of {}: {}", i, path.display(), resource_content);
             }
         }
         std::thread::sleep(std::time::Duration::from_secs(1));
