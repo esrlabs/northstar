8ab7ff6660350125f0d0ee0141808045b7507eee
Felix Obenhuber <felix.obenhuber@esrlabs.com>
2021-03-09 15:29:20 +0100

    Fix license header

--- northstar_tests/src/logger.rs
+++ northstar_tests/src/logger.rs
@@ -1,7 +1,7 @@
 // Copyright (c) 2019 - 2020 ESRLabs
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
-//   you may not use thpub(crate) is file except in compliance with the License.
+//   you may not use this file except in compliance with the License.
 //   You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
