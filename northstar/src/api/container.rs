6bda58f3b88e1578db1e4a681e3c2121fb002bd6
Alfonso Ros <alfonso.ros@esrlabs.com>
2021-03-10 10:47:41 +0100

    Fix dependency from api to runtime
    After the changes for lazy mounting, a dependency from api -> runtime
    was introduced. The api models for ExitCode and Notification used the
    homologue in runtime. Additionally the api model used the
    runtime::continer module.
    
    This pr solves this issue by:
    - Inverting the dependency for notification and exitcode.
    - Moves runtime::container module to api

--- northstar/src/api/container.rs
+++ northstar/src/api/container.rs
@@ -12,7 +12,7 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
-use super::{Name, Version};
+use npk::manifest::{Name, Version};
 use serde::{Deserialize, Serialize};
 use std::{
     convert::TryFrom,
