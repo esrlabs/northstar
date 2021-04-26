83570bd97b6361227a7dafb0fb50ffca576328ef
Felix Obenhuber <felix.obenhuber@esrlabs.com>
2021-04-26 15:43:23 +0200

    Refactor integration test for `aarch64-linux-android`
    * add runner configuration for `aarch64-linux-android`
    * build test npks at build time
    * include the test npk into the test binary to be workspace independant
    * change test console address from `/tmp` to $TMPDIR
    * fix runtime configuration for `aarch64-linux-android`

---
+++ northstar_tests/build.rs
@@ -0,0 +1,70 @@
+// Copyright (c) 2019 - 2020 ESRLabs
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+use escargot::CargoBuild;
+use std::{env, fs, path::Path};
+use tokio::runtime::Builder;
+
+const CARGO_MANIFEST: &str = "test_container/Cargo.toml";
+const TEST_CONTAINER_MANIFEST: &str = "test_container/manifest.yaml";
+const TEST_RESOURCE_MANIFEST: &str = "test_resource/manifest.yaml";
+const KEY: &str = "../examples/keys/northstar.key";
+
+fn main() {
+    let tmpdir = tempfile::TempDir::new().expect("Failed to create tmpdir");
+    let npk = tmpdir.path().join("npk");
+    let root = npk.join("root");
+    fs::create_dir_all(&root).expect("Failed to create npk root");
+
+    // Build the test container binary
+    let bin = CargoBuild::new()
+        .manifest_path(CARGO_MANIFEST)
+        .current_release()
+        .target(env::var("TARGET").unwrap())
+        .target_dir(tmpdir.path())
+        .run()
+        .expect("Failed to build")
+        .path()
+        .to_owned();
+    fs::copy(&bin, &root.join("test_container")).expect("failed to copy bin");
+    fs::copy(&bin, "/tmp/asdf").expect("failed to copy bin");
+
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let runtime = Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .expect("Failed to start RT");
+
+    runtime.block_on(async {
+        // Pack test container npk
+        npk::npk::pack(
+            Path::new(TEST_CONTAINER_MANIFEST),
+            &npk.join("root"),
+            Path::new(&out_dir),
+            Some(Path::new(KEY)),
+        )
+        .await
+        .expect("Failed to create test container npk");
+
+        // Pack test resource npk
+        npk::npk::pack(
+            Path::new(TEST_RESOURCE_MANIFEST),
+            &Path::new("test_resource").join("root"),
+            Path::new(&out_dir),
+            Some(Path::new(KEY)),
+        )
+        .await
+        .expect("Failed to create test resource npk");
+    });
+}
--- northstar_tests/src/test_container.rs
+++ northstar_tests/src/test_container.rs
@@ -12,87 +12,36 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
-use async_once::AsyncOnce;
-use color_eyre::eyre::WrapErr;
-use escargot::CargoBuild;
 use lazy_static::lazy_static;
-use log::debug;
 use northstar::api::model::Container;
-use npk::npk;
 use std::{
     convert::TryInto,
     path::{Path, PathBuf},
 };
 use tempfile::TempDir;
-use tokio::fs;
+
+lazy_static! {
+    static ref TMPDIR: TempDir = TempDir::new().expect("Failed to create tmpdir");
+    static ref TEST_CONTAINER_NPK: PathBuf = {
+        let src = include_bytes!(concat!(env!("OUT_DIR"), "/test_container-0.0.1.npk"));
+        let npk = TMPDIR.path().join("test-container.npk");
+        std::fs::write(&npk, src).expect("Failed to dump npk");
+        npk
+    };
+    static ref TEST_RESOURCE_NPK: PathBuf = {
+        let src = include_bytes!(concat!(env!("OUT_DIR"), "/test_resource-0.0.1.npk"));
+        let npk = TMPDIR.path().join("test-resource.npk");
+        std::fs::write(&npk, src).expect("Failed to dump npk");
+        npk
+    };
+}
 
 pub const TEST_CONTAINER: &str = "test_container:0.0.1:test";
 pub const TEST_RESOURCE: &str = "test_resource:0.0.1:test";
-const MANIFEST: &str = "northstar_tests/test_container/Cargo.toml";
-
-lazy_static! {
-    static ref REPOSITORY: TempDir = TempDir::new().unwrap();
-    static ref TEST_CONTAINER_NPK: AsyncOnce<PathBuf> = AsyncOnce::new(async {
-        let package_dir = TempDir::new().unwrap();
-        let root = package_dir.path().join("root");
-
-        debug!("Building test container binary");
-        let binary_path = CargoBuild::new()
-            .manifest_path(MANIFEST)
-            .run()
-            .wrap_err("Failed to build the test_container")
-            .unwrap()
-            .path()
-            .to_owned();
-
-        debug!("Creating test container binary npk");
-        fs::create_dir_all(&root).await.unwrap();
-
-        async fn copy_file(file: &Path, dir: &Path) {
-            assert!(file.is_file());
-            assert!(dir.is_dir());
-            let filename = file.file_name().unwrap();
-            fs::copy(file, dir.join(filename)).await.unwrap();
-        }
-
-        copy_file(&binary_path, &root).await;
-        copy_file(
-            Path::new("northstar_tests/test_container/manifest.yaml"),
-            package_dir.path(),
-        )
-        .await;
-
-        npk::pack(
-            package_dir
-                .path()
-                .join("manifest")
-                .with_extension("yaml")
-                .as_path(),
-            package_dir.path().join("root").as_path(),
-            REPOSITORY.path(),
-            Some(Path::new("examples/keys/northstar.key")),
-        )
-        .await
-        .unwrap();
-
-        REPOSITORY.path().join("test_container-0.0.1.npk")
-    });
-    static ref TEST_RESOURCE_NPK: AsyncOnce<PathBuf> = AsyncOnce::new(async {
-        npk::pack(
-            Path::new("northstar_tests/test_resource/manifest.yaml"),
-            Path::new("northstar_tests/test_resource/root"),
-            REPOSITORY.path(),
-            Some(Path::new("examples/keys/northstar.key")),
-        )
-        .await
-        .unwrap();
-        REPOSITORY.path().join("test_resource-0.0.1.npk")
-    });
-}
 
 /// Path to the test container npk
 pub async fn test_container_npk() -> &'static Path {
-    &TEST_CONTAINER_NPK.get().await
+    &TEST_CONTAINER_NPK
 }
 
 /// Test container key
@@ -102,5 +51,5 @@ pub fn test_container() -> Container {
 
 // Path to the test resource npk
 pub async fn test_resource_npk() -> &'static Path {
-    &TEST_RESOURCE_NPK.get().await
+    &TEST_RESOURCE_NPK
 }
