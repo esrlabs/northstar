8e6b7b47fbaf551d9ed56447acc68af66b43c68b
Felix Obenhuber <felix@obenhuber.de>
2021-09-28 14:23:08 +0200

    Logger tag size is only releavant for non Android
    Guard the TAG_SIZE global with a cfg directive.

--- main/src/logger.rs
+++ main/src/logger.rs
@@ -1,17 +1,3 @@
-use std::sync::atomic::AtomicUsize;
-
-// Copyright (c) 2021 E.S.R.Labs. All rights reserved.
-//
-// NOTICE:  All information contained herein is, and remains
-// the property of E.S.R.Labs and its suppliers, if any.
-// The intellectual and technical concepts contained herein are
-// proprietary to E.S.R.Labs and its suppliers and may be covered
-// by German and Foreign Patents, patents in process, and are protected
-// by trade secret or copyright law.
-// Dissemination of this information or reproduction of this material
-// is strictly forbidden unless prior written permission is obtained
-// from E.S.R.Labs.
-
 /// Initialize the logger
 #[cfg(target_os = "android")]
 pub fn init() {
@@ -22,7 +8,8 @@ pub fn init() {
         .init();
 }
 
-static TAG_SIZE: AtomicUsize = AtomicUsize::new(20);
+#[cfg(not(target_os = "android"))]
+static TAG_SIZE: std::sync::atomic::AtomicUsize = std::sync::atomic::AtomicUsize::new(20);
 
 /// Initialize the logger
 #[cfg(not(target_os = "android"))]
