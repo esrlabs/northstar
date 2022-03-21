c30fda32e699c5e16215f264e422f4d4bc1f5c18
Felix Obenhuber <felix@obenhuber.de>
2022-03-21 08:44:08 +0100

    Fix env variable name
    The env variable that is used to pass the container version from the
    runtime to the container has a `NORTHSTAR_` prefix.

--- examples/seccomp/src/main.rs
+++ examples/seccomp/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let version = std::env::var("VERSION").unwrap_or_else(|_| "unknown".into());
+    let version = std::env::var("NORTHSTAR_VERSION").unwrap_or_else(|_| "unknown".into());
     loop {
         println!("Hello from the seccomp example version {}!", version);
         std::thread::sleep(std::time::Duration::from_secs(5));
