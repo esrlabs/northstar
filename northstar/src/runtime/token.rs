3873d09b68e94c534cc264414bb52e6ebe5f97c5
Felix Obenhuber <felix@obenhuber.de>
2022-04-13 14:40:04 +0200

    Extend token with conversion and verification result

--- northstar/src/runtime/token.rs
+++ northstar/src/runtime/token.rs
@@ -4,12 +4,16 @@ use hmac::{
 };
 use lazy_static::lazy_static;
 use rand_core::{OsRng, RngCore};
-use serde::{de::Visitor, Deserialize, Serialize, Serializer};
 use sha2::Sha256;
-use std::{fmt, time::Instant};
+use std::{
+    fmt,
+    time::{self, Instant},
+};
+
+use crate::api;
 
 // Tokens are valid for one minute
-pub const TOKEN_EXPIRED_THRESHOLD: Timestamp = 60_000; // 60 seconds
+const TOKEN_EXPIRED_THRESHOLD: time::Duration = time::Duration::from_secs(60);
 
 lazy_static! {
     static ref MAC_KEY: [u8; 32] = {
@@ -17,101 +21,101 @@ lazy_static! {
         OsRng.fill_bytes(&mut key);
         key
     };
-    static ref FIRST_TOKEN_TIME: Instant = Instant::now();
-}
-
-pub type Timestamp = u128;
-
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-pub struct Token {
-    time: Timestamp,
-    hmac: Hmac,
+    /// Use Instant as a monotonic clock source
+    static ref START_TIMESTAMP: Instant = Instant::now();
 }
 
 type HmacSha256 = hmac::Hmac<Sha256>;
+type Hmac = CtOutput<HmacSha256>;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub(crate) enum VerificationResult {
+    /// Verification succeeded
+    Valid,
+    /// Verification failed
+    Invalid,
+    /// Token is expired
+    Expired,
+}
+
+/// Token instance
+#[derive(Clone, PartialEq)]
+pub(crate) struct Token {
+    time: time::Duration,
+    hmac: Hmac,
+}
 
 impl Token {
-    pub fn new(usage: &str) -> Token {
-        let time = Token::current_time();
-        let hmac = Token::calculate_hmac(&time, usage);
+    /// Create a new token
+    pub fn new<T: AsRef<[u8]>>(usage: T) -> Token {
+        let now = now();
+        let hmac = calculate_hmac(&now, usage);
+        Token { time: now, hmac }
+    }
+
+    /// Verify that `usage` matches the token
+    pub fn verify<T: AsRef<[u8]>>(&self, usage: T) -> VerificationResult {
+        if now()
+            .checked_sub(self.time)
+            .map_or(true, |age| age > TOKEN_EXPIRED_THRESHOLD)
+        {
+            VerificationResult::Expired
+        } else if calculate_hmac(&self.time, usage) == self.hmac {
+            VerificationResult::Valid
+        } else {
+            VerificationResult::Invalid
+        }
+    }
+}
+
+fn now() -> time::Duration {
+    let now = Instant::now().duration_since(*START_TIMESTAMP);
+    time::Duration::from_secs(now.as_secs()) // round to seconds
+}
+
+fn calculate_hmac<T: AsRef<[u8]>>(time: &time::Duration, usage: T) -> Hmac {
+    let mut hasher =
+        HmacSha256::new_from_slice(MAC_KEY.as_slice()).expect("Failed to create SHA-256 hasher");
+    hasher.update(&time.as_millis().to_be_bytes());
+    hasher.update(usage.as_ref());
+    hasher.finalize()
+}
+
+impl From<[u8; 40]> for Token {
+    fn from(bytes: [u8; 40]) -> Self {
+        let mut time = [0u8; 8];
+        time.copy_from_slice(&bytes[..8]);
+        let time = time::Duration::from_secs(u64::from_be_bytes(time));
+        let hmac = CtOutput::<HmacSha256>::new(GenericArray::clone_from_slice(&bytes[8..]));
         Token { time, hmac }
     }
+}
 
-    pub fn verify(&self, usage: &str) -> bool {
-        let hmac = Token::calculate_hmac(&self.time, usage);
-        let is_authentic = self.hmac == hmac; // constant time comparison ensured by CtOutput
-        let is_expired = Token::current_time()
-            .checked_sub(self.time)
-            .map_or(true, |age| age > TOKEN_EXPIRED_THRESHOLD);
-        is_authentic && !is_expired
-    }
-
-    pub fn write<T: std::io::Write>(&self, output: &mut T) -> std::io::Result<()> {
-        let time = self.time.to_be_bytes();
-        let hmac = self.hmac.clone().into_bytes();
-        output.write_all(&time)?;
-        output.write_all(&hmac)?;
-        Ok(())
-    }
-
-    fn calculate_hmac(time: &Timestamp, usage: &str) -> Hmac {
-        let mut hasher = HmacSha256::new_from_slice(MAC_KEY.as_slice())
-            .expect("Failed to create SHA-256 hasher");
-        hasher.update(&time.to_be_bytes());
-        hasher.update(usage.as_bytes());
-        Hmac(hasher.finalize())
-    }
-
-    fn current_time() -> Timestamp {
-        std::time::SystemTime::now()
-            .duration_since(std::time::UNIX_EPOCH)
-            .expect("Clock is earlier than UNIX Epoch")
-            .as_millis()
+impl From<Token> for [u8; 40] {
+    fn from(token: Token) -> Self {
+        let mut bytes = [0u8; 40];
+        bytes[..8].copy_from_slice(&token.time.as_secs().to_be_bytes());
+        bytes[8..].copy_from_slice(&token.hmac.into_bytes());
+        bytes
     }
 }
 
-#[derive(PartialEq)]
-struct Hmac(CtOutput<HmacSha256>);
-
-impl fmt::Debug for Hmac {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "\"{}\"", hex::encode(self.0.clone().into_bytes()))
+impl fmt::Debug for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Token")
+            .field("time", &self.time)
+            .field("hmac", &self.hmac.clone().into_bytes())
+            .finish()
     }
 }
 
-impl std::ops::Deref for Hmac {
-    type Target = CtOutput<HmacSha256>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl Serialize for Hmac {
-    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        serializer.serialize_str(&hex::encode(self.0.clone().into_bytes()))
-    }
-}
-
-impl<'de> Deserialize<'de> for Hmac {
-    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        struct HashVisitor;
-
-        impl<'de> Visitor<'de> for HashVisitor {
-            type Value = CtOutput<HmacSha256>;
-            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-                formatter.write_str("a 32 byte sequence")
-            }
-
-            fn visit_str<E: serde::de::Error>(self, v: &str) -> Result<Self::Value, E> {
-                let decoded = hex::decode(v).map_err(serde::de::Error::custom)?;
-                Ok(CtOutput::<HmacSha256>::new(GenericArray::clone_from_slice(
-                    &decoded,
-                )))
-            }
+impl From<VerificationResult> for api::model::VerificationResult {
+    fn from(result: VerificationResult) -> Self {
+        match result {
+            VerificationResult::Valid => api::model::VerificationResult::Valid,
+            VerificationResult::Invalid => api::model::VerificationResult::Invalid,
+            VerificationResult::Expired => api::model::VerificationResult::Expired,
         }
-
-        Ok(Hmac(deserializer.deserialize_str(HashVisitor)?))
     }
 }
 
@@ -119,11 +123,11 @@ impl<'de> Deserialize<'de> for Hmac {
 mod test {
     use super::*;
 
-    const USAGE: &str = "MQTT";
+    const USAGE: &str = "hello";
 
     #[test]
     fn verify_new_token() {
-        assert!(Token::new(USAGE).verify(USAGE));
+        assert_eq!(Token::new(USAGE).verify(USAGE), VerificationResult::Valid);
     }
 
     #[test]
@@ -131,22 +135,21 @@ mod test {
         let mut recent_token = Token::new(USAGE);
         recent_token.time = recent_token
             .time
-            .checked_sub(1000 * 10) // 10 seconds into the past
+            .checked_sub(time::Duration::from_secs(10)) // 10 seconds into the past
             .expect("Failed to create future timestamp");
-        recent_token.hmac = Token::calculate_hmac(&recent_token.time, USAGE); // Fix HMAC for changed timestamp
-        assert!(recent_token.verify(USAGE));
+        recent_token.hmac = calculate_hmac(&recent_token.time, USAGE); // Fix HMAC for changed timestamp
+        assert_eq!(recent_token.verify(USAGE), VerificationResult::Valid);
     }
 
     #[test]
     fn verify_old_token() {
-        const ONE_HOUR_IN_MS: Timestamp = 24 * 60 * 60 * 1000;
         let mut old_token = Token::new(USAGE);
         old_token.time = old_token
             .time
-            .checked_sub(ONE_HOUR_IN_MS)
+            .checked_sub(time::Duration::from_secs(3600)) // 1 hour into the past
             .expect("Failed to create future timestamp");
-        old_token.hmac = Token::calculate_hmac(&old_token.time, USAGE); // Fix HMAC for changed timestamp
-        assert!(!old_token.verify(USAGE));
+        old_token.hmac = calculate_hmac(&old_token.time, USAGE); // Fix HMAC for changed timestamp
+        assert_eq!(old_token.verify(USAGE), VerificationResult::Expired);
     }
 
     #[test]
@@ -154,9 +157,9 @@ mod test {
         let mut future_token = Token::new(USAGE);
         future_token.time = future_token
             .time
-            .checked_add(1)
+            .checked_add(time::Duration::from_millis(1))
             .expect("Failed to create future timestamp");
-        assert!(!future_token.verify("XMPP"));
+        assert_eq!(future_token.verify(USAGE), VerificationResult::Expired);
     }
 
     #[test]
@@ -165,36 +168,24 @@ mod test {
         let mut broken_mac = broken_token.hmac.clone().into_bytes().to_vec();
         broken_mac[0] = broken_mac[0].overflowing_add(1).0;
         let broken_mac: [u8; 32] = broken_mac.try_into().unwrap();
-        broken_token.hmac = Hmac(CtOutput::<HmacSha256>::new(GenericArray::clone_from_slice(
-            &broken_mac,
-        )));
-        assert!(!broken_token.verify(USAGE));
+        broken_token.hmac =
+            CtOutput::<HmacSha256>::new(GenericArray::clone_from_slice(&broken_mac));
+        assert_eq!(broken_token.verify(USAGE), VerificationResult::Invalid);
     }
 
     #[test]
     fn verify_wrong_usage() {
-        assert!(!Token::new(USAGE).verify("XMPP"));
+        assert_eq!(
+            Token::new(USAGE).verify("XMPP"),
+            VerificationResult::Invalid
+        );
     }
 
     #[test]
-    fn write_token() {
-        let mut output = [0u8; 40];
-        assert!(Token::new(USAGE)
-            .write(&mut std::io::BufWriter::new(output.as_mut_slice()))
-            .is_ok());
-    }
-
-    #[test]
-    fn write_token_to_short_buffer() {
-        let mut output = [0u8; 39];
-        assert!(Token::new(USAGE).write(&mut output.as_mut_slice()).is_err());
-    }
-
-    #[test]
-    fn token_serde_roundtrip() {
-        let token = Token::new(USAGE);
-        let serialized = serde_json::to_string(&token).unwrap();
-        let deserialized: Token = serde_json::from_str(&serialized).unwrap();
-        assert_eq!(token, deserialized);
+    fn byte_array_roundtrip() {
+        let original = Token::new(USAGE);
+        let bytes: [u8; 40] = original.clone().into();
+        let token: Token = bytes.into();
+        assert_eq!(original, token);
     }
 }
