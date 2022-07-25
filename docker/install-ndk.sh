9ce1a46ce909419fcb8e1fb4247f184d04e78d65
Felix Obenhuber <felix@obenhuber.de>
2022-07-25 09:18:49 +0200

    Update docker base images

--- docker/install-ndk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl --retry 3 -sSfL "https://dl.google.com/android/repository/android-ndk-r19c-linux-x86_64.zip" -O
-unzip -q android-ndk-*.zip
-rm android-ndk-*.zip
-mv android-ndk-r19c/toolchains/llvm/prebuilt/linux-x86_64 /android-ndk
-rm -r android-ndk-r19c
