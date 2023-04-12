14c23d2bba8c63d2997eebd21bf13b2b4cad88d1
Felix Obenhuber <felix.obenhuber@esrlabs.com>
2023-04-12 09:55:45 +0200

    Syncronize logs
    Runtime logs are sent from different processes and need to be aligend.
    Extend the example main with a toy logger via Unix datagram sockets.

--- northstar/src/logger.rs
+++ northstar/src/logger.rs
@@ -8,86 +8,149 @@ pub fn init() {
         .init();
 }
 
-#[cfg(not(target_os = "android"))]
-static TAG_SIZE: std::sync::atomic::AtomicUsize = std::sync::atomic::AtomicUsize::new(28);
-
-/// Initialize the logger
+/// Initialize the logger. To synchronize the log output of the different process, the records
+/// are sent via a unix datagram socket to a thread that prints the log records.
 #[cfg(not(target_os = "android"))]
 pub fn init() {
-    use env_logger::fmt::Color;
-    use std::{io::Write, sync::atomic::Ordering};
+    use console::{style, Color};
+    use log::{Level, Log};
+    use serde::{Deserialize, Serialize};
+    use std::{os::unix::net::UnixDatagram, thread};
+    use time::{format_description::FormatItem, macros::format_description, OffsetDateTime};
 
-    fn color(target: &str) -> Color {
-        // Some colors are hard to read on (at least) dark terminals
-        // and I consider some others as ugly ;-)
-        let hash = target.bytes().fold(42u8, |c, x| c ^ x);
-        Color::Ansi256(match hash {
-            c @ 0..=1 => c + 2,
-            c @ 16..=21 => c + 6,
-            c @ 52..=55 | c @ 126..=129 => c + 4,
-            c @ 163..=165 | c @ 200..=201 => c + 3,
-            c @ 207 => c + 1,
-            c @ 232..=240 => c + 9,
-            c => c,
-        })
+    const TIMESETAMP_FORMAT: &[FormatItem<'static>] =
+        format_description!("[hour]:[minute]:[second].[subsecond digits:3]");
+
+    /// A log record that is serialized and sent to the logger thread.
+    #[derive(Debug, Serialize, Deserialize)]
+    struct Record {
+        /// Timestamp.
+        timestamp: OffsetDateTime,
+        /// Log level.
+        level: Level,
+        /// Thread group id.
+        tgid: u32,
+        /// Taget string.
+        target: String,
+        /// Log message.
+        message: String,
     }
 
-    let mut builder = env_logger::Builder::new();
-    builder.parse_filters("debug");
+    /// Generate a color of `self`.
+    trait HashColor {
+        fn color(&self) -> Color;
+    }
 
-    builder.format(|buf, record| {
-        let timestamp = buf.timestamp_millis().to_string();
-        let timestamp = timestamp.strip_suffix('Z').unwrap();
+    impl HashColor for &str {
+        fn color(&self) -> Color {
+            let hash = self.bytes().fold(42u8, |c, x| c ^ x);
+            Color::Color256(hash)
+        }
+    }
 
-        let mut level = buf.default_level_style(record.metadata().level());
-        level.set_bold(true);
-        let level = level.value(record.metadata().level().as_str());
+    impl HashColor for u32 {
+        fn color(&self) -> Color {
+            // Some colors are hard to read on (at least) dark terminals
+            // and I consider some others as ugly ;-)
+            let color = match *self as u8 {
+                c @ 0..=1 => c + 2,
+                c @ 16..=21 => c + 6,
+                c @ 52..=55 | c @ 126..=129 => c + 4,
+                c @ 163..=165 | c @ 200..=201 => c + 3,
+                c @ 207 => c + 1,
+                c @ 232..=240 => c + 9,
+                c => c,
+            };
+            Color::Color256(color)
+        }
+    }
 
-        let pid = std::process::id().to_string();
-        let mut pid_style = buf.style();
-        pid_style.set_color(color(&pid));
+    let (logger, client) =
+        UnixDatagram::pair().expect("failed to create unix datagram socket pair");
 
-        if let Some(target) = Option::from(record.target().is_empty())
-            .map(|_| record.target())
-            .or_else(|| record.module_path())
-            .map(|module_path| {
-                module_path
-                    .strip_prefix("northstar::")
-                    .unwrap_or(module_path)
-            })
-            .map(|module_path| {
-                module_path
-                    .strip_prefix("northstar_runtime::")
-                    .unwrap_or(module_path)
-            })
-        {
-            let mut tag_style = buf.style();
-            TAG_SIZE.fetch_max(target.len(), Ordering::SeqCst);
-            let tag_size = TAG_SIZE.load(Ordering::SeqCst);
-            tag_style.set_color(color(target));
+    // Spawn a thread that reads from the socket and prints the log records.
+    thread::spawn(move || {
+        let mut buffer = vec![0u8; 64 * 1024];
+        let mut target_size = 30;
+        loop {
+            let record = logger
+                .recv(&mut buffer)
+                .map(|n| &buffer[..n])
+                .expect("logging socket error");
+            let record =
+                bincode::deserialize::<Record>(record).expect("failed to deserialize log record");
+            let timestamp = record
+                .timestamp
+                .format(TIMESETAMP_FORMAT)
+                .expect("failed to format timestamp");
+            let target = style(&record.target)
+                .bold()
+                .fg(record.target.as_str().color());
+            let level_color = match record.level {
+                Level::Error => Color::Red,
+                Level::Warn => Color::Yellow,
+                Level::Info => Color::Green,
+                Level::Debug => Color::Color256(243),
+                Level::Trace => Color::White,
+            };
+            let level = style(record.level).bold().fg(level_color);
+            let message = record.message;
+            target_size = target_size.max(record.target.len());
 
-            writeln!(
-                buf,
-                "{} {:>s$} {}  {:<5}: {}",
-                timestamp,
-                tag_style.value(target),
-                pid_style.value("⬤"),
-                level,
-                record.args(),
-                s = tag_size,
-            )
-        } else {
-            writeln!(
-                buf,
-                "{} {} {}  {:<5}: {}",
-                timestamp,
-                " ".repeat(TAG_SIZE.load(Ordering::SeqCst)),
-                pid_style.value("⬤"),
-                level,
-                record.args(),
-            )
+            let tgid = style("⬤").fg(record.tgid.color());
+            println!(
+                "{timestamp} {target:>s$} {tgid}  {level:<5}: {message}",
+                s = target_size
+            );
         }
     });
 
-    builder.init()
+    struct Logger(UnixDatagram);
+
+    impl Log for Logger {
+        fn enabled(&self, metadata: &log::Metadata) -> bool {
+            metadata.level() <= log::max_level()
+        }
+
+        fn log(&self, record: &log::Record) {
+            let timestamp = OffsetDateTime::now_utc();
+            let level = record.level();
+            let tgid = std::process::id();
+            let target = Option::from(record.target().is_empty())
+                .map(|_| record.target())
+                .or_else(|| record.module_path())
+                .map(|module_path| {
+                    module_path
+                        .strip_prefix("northstar::")
+                        .unwrap_or(module_path)
+                })
+                .map(|module_path| {
+                    module_path
+                        .strip_prefix("northstar_runtime::")
+                        .unwrap_or(module_path)
+                })
+                .unwrap_or_default()
+                .to_owned();
+            let message = record.args().to_string();
+
+            let record = Record {
+                timestamp,
+                level,
+                tgid,
+                target,
+                message,
+            };
+
+            let message = bincode::serialize(&record).expect("failed to serialize log record");
+            self.0
+                .send(&message)
+                .map(drop)
+                .expect("failed to send log record");
+        }
+
+        fn flush(&self) {}
+    }
+
+    log::set_boxed_logger(Box::new(Logger(client))).expect("failed to set logger");
+    log::set_max_level(log::LevelFilter::Debug);
 }
