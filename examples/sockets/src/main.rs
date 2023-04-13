1397a11ef01027a199d8945d0f8e139f57cf69fe
Felix Obenhuber <felix.obenhuber@esrlabs.com>
2023-04-13 16:40:23 +0200

    Extend socket example

--- examples/sockets/src/main.rs
+++ examples/sockets/src/main.rs
@@ -1,4 +1,3 @@
-use anyhow::Result;
 use std::{
     env,
     os::{fd::FromRawFd, unix::net::UnixDatagram},
@@ -6,33 +5,41 @@ use std::{
     thread,
 };
 
-fn main() -> Result<()> {
+fn main() {
     let server = thread::spawn(server);
     let client = thread::spawn(client);
 
-    server.join().expect("server join error")?;
-    client.join().expect("client join error")?;
-    Ok(())
+    server.join().expect("server join error");
+    client.join().expect("client join error");
 }
 
-fn server() -> Result<()> {
-    let stream_fd = env::var("NORTHSTAR_SOCKET_hello")?.parse()?;
+fn server() {
+    let stream_fd = env::var("NORTHSTAR_SOCKET_hello")
+        .expect("missing socket variable")
+        .parse()
+        .expect("failed to parse socket variable");
     let socket = unsafe { UnixDatagram::from_raw_fd(stream_fd) };
     let mut buf = [0; 1024];
-    let buf = socket.recv(&mut buf).map(|n| &buf[..n])?;
-    println!("Received {}", String::from_utf8(buf.to_vec())?.trim());
-    Ok(())
+    let buf = socket
+        .recv(&mut buf)
+        .map(|n| &buf[..n])
+        .expect("failed to receive");
+    println!(
+        "Received {}",
+        String::from_utf8(buf.to_vec())
+            .expect("invalid data")
+            .trim()
+    );
 }
 
 // The socket directory is bind mounted to /unix-sockets. See manifest.yaml.
 // The code below normally runs in a different container...
-fn client() -> Result<()> {
+fn client() {
     // Socket path.
     let path = Path::new("/unix-sockets").join("sockets").join("hello");
 
     println!("Connecting to {}", path.display());
-    let socket = UnixDatagram::unbound()?;
-    socket.connect(path)?;
-    socket.send("Hello!".as_bytes())?;
-    Ok(())
+    let socket = UnixDatagram::unbound().expect("failed to create socket");
+    socket.connect(path).expect("failed to connect");
+    socket.send("Hello!".as_bytes()).expect("failed to send");
 }
